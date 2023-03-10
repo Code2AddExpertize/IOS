585128149f55a52016a37ab82726ae2d7c5cbe0c
Ashish Kumar Singh <ashishkumarsingh@Ashishs-MacBook-Pro.local>
2023-03-10 16:10:09 +0530

    Learning Project this far

---
+++ I am Rich/I am Rich/ViewController.swift
@@ -0,0 +1,18 @@
+//
+//  ViewController.swift
+//  I am Rich
+//
+//  Created by Ashish Kumar Singh on 06/11/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.¡™™
+    }
+}
+
+
